325f32c82e19c33fad035415ebc11459bb68a67c
fpumir <fpumir@terredeweb.fr>
2014-02-21 11:42:43 +0100

    bootstrap
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- mobileApp/Controllers/Mobile_controller.php
+++ mobileApp/Controllers/Mobile_controller.php
@@ -4,22 +4,38 @@ class Mobile_controller extends Controller{
 
   public function __construct(){
     parent::__construct();
+    $f3=\Base::instance();
+    if(!$f3->get('SESSION.id')&&$f3->get('PATTERN')!='/mobile')
+      $f3->reroute('/mobile');
     $this->tpl=array();
   }
   
   
   public function home($f3){
+    $this->tpl['sync']='home.html';
     switch($f3->get('VERB')){
-      case 'GET':
-        $this->tpl['sync']='home.html';
-      break;
       case 'POST':
-        
+        $user=$this->model->login(array(
+          'username'=>$f3->get('POST.username'),
+          'password'=>$f3->get('POST.password')
+        ));
+        if($user){
+          $f3->set('SESSION.username',$user->username);
+          $f3->set('SESSION.id',$user->id);
+          $f3->reroute('/mobile/membre');
+        }
+        else{
+          $f3->set('error','Oops, vos identifiants sont erronés.');
+        }
       break;
     }
     
   }
   
+  
+  public function membre($f3){
+    $this->tpl['sync']='membre.html';
+  }
  
   
       
--- mobileApp/Models/Mobile_model.php
+++ mobileApp/Models/Mobile_model.php
@@ -3,73 +3,16 @@ class Mobile_model extends Model{
     
 private $mapper;
 
-  function __construct(){
+  public function __construct(){
     parent::__construct();
    
   }
+  
+  public function login($params){
+    return $this->getMapper('users')->load(array('username=? and password=?',$params['username'],md5($params['password'])));
+  }
 
-  function getUsers($params){
-    return $this->mapper->find(array('promo=?',$params['promo']),array('order'=>'lastname'));
-  }
   
-  function getFavUsers($params){
-    $map=$this->getMapper('favorite');
-    return $map->find(array('logId=?',$params['logId']),array('order'=>'lastname'));
-  }
-  
-  function getFavorites($params){
-    $map=$this->getMapper('wififav');
-    return $map->find(array('logId=?',$params['logId']));
-  }
-  
-  function getUser($params){
-    return $this->mapper->load(array('id=?',$params['id']));
-  }
-  
-  function searchUsers($params){
-    $query='(firstname like "%'.$params['keywords'].'%" or lastname  like "%'.$params['keywords'].'%")';
-    $query.=$params['filter']?' and promo="'.$params['filter'].'"':'';
-    return $this->mapper->find($query,array('order'=>'lastname'));
-    
-  }
-  
-  function favorite($params){
-    $map=$this->getMapper('wififav');
-    $favorite=$map->load(array('favId=? and logId=?',$params['favId'],$params['logId']));
-    
-    if($favorite){
-      $favorite->erase();
-      return false;
-    }
-    $map->logId=$params['logId'];
-    $map->favId=$params['favId'];
-    $map->save();
-    return true;
-  }
-  
-  function recordFile($params){
-    $map=$this->getMapper('files');
-    $map->userId=$params['userId'];
-    $map->logId=$params['logId'];
-    $map->filePath=$params['filePath'];
-    $map->name=$params['name'];
-    $map->save();
-    return true;
-  }
-  
-  function getFiles($params){
-    $map=$this->getMapper('file');
-    return $map->find(array('userId=?',$params['id']));
-
-  }
-  function getFile($params){
-    $map=$this->getMapper('files');
-    return $map->load(array('id=?',$params['id']));
-  }
-  
-  function signin($params){
-    return $this->mapper->findone(array('userId=?',$params['login']));
-  }
   
   
 }
